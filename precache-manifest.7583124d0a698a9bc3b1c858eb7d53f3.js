8b4bbe6625f1cc7122c29b5eb69c880c26ccc490
Anthony Hung Nguyen <nguyenphuchung1999@gmail.com>
2019-03-05 22:16:59 +0700

    Updates

---
+++ precache-manifest.7583124d0a698a9bc3b1c858eb7d53f3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "d314bcdce46702913dd1",
+    "url": "/pokedex/static/css/main.789bd1d8.chunk.css"
+  },
+  {
+    "revision": "d314bcdce46702913dd1",
+    "url": "/pokedex/static/js/main.d314bcdc.chunk.js"
+  },
+  {
+    "revision": "c92f8f4bce56a57764ce",
+    "url": "/pokedex/static/js/runtime~main.c92f8f4b.js"
+  },
+  {
+    "revision": "7feee2be329f11b9d047",
+    "url": "/pokedex/static/css/2.b0781358.chunk.css"
+  },
+  {
+    "revision": "7feee2be329f11b9d047",
+    "url": "/pokedex/static/js/2.7feee2be.chunk.js"
+  },
+  {
+    "revision": "db8c003f053ad576d84f9e986e628140",
+    "url": "/pokedex/static/media/Tieng.db8c003f.woff"
+  },
+  {
+    "revision": "7960b3c23c2d312d24d45e2d8551f80e",
+    "url": "/pokedex/static/media/Pokemon Solid.7960b3c2.woff"
+  },
+  {
+    "revision": "f0c29fcce86cd1c62e4459be7d42da7c",
+    "url": "/pokedex/static/media/Pokemon Hollow.f0c29fcc.woff"
+  },
+  {
+    "revision": "cead23a5dea18352de34aff674bcaa4a",
+    "url": "/pokedex/index.html"
+  }
+];
